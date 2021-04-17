e2d344314de9bdbe3754f321fc4c055b89f6e211
jstgermain123 <63817796+jstgermain123@users.noreply.github.com>
2021-04-17 19:37:22 -0400

    v1
    Summary Statistics Added, set correct data types

--- Cleanup_Code.R
+++ Cleanup_Code.R
@@ -1,11 +1,26 @@
 #load data
 data <- read.csv(file.choose(), sep=",", header = TRUE )
 
+# Set Correct Data Types
+data$id <- as.character(data$id)
+data$yr_renovated <- as.character(data$yr_renovated)
+data$yr_built <- as.character(data$yr_built)
+data$zipcode <- as.character(data$zipcode)
+
+# Get Summary Stats
+summary(data)
+
+# Find NAs
+new_DF <- data[rowSums(is.na(data)) > 0,]
+
+
+
+
 # Code that cane be used to replace N/A's
 # use for variables as needed
 data$Deposit.Amount[(is.na(data$Completed) == TRUE)] <- 100
 
 # Code for correlation plot
-cordata <- data[,c(4,6,11)] # use this to select columns you need for correlation plot (numerical onlu)
+cordata <- data[,c(4,6,11)] # use this to select columns you need for correlation plot (numerical only)
 res <- cor(cordata) 
 round(res, 2)
